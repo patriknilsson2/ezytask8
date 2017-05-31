74894e8698024f8a8eddfcd56f8b6a2daa5b6025
patriknilsson2 <patteee@gmail.com>
2017-05-31 16:36:11 +0200

    initial

---
+++ ezytask8/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ezytask8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<List<ForexData>> CurrencyData()
+        {
+            List<ForexData> forexDataList = new List<ForexData>();
+            await Task.Run(() =>
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                    XDocument xmlDoc = XDocument.Load("http://www.forex.se/ratesxml.asp?id=492");
+                    forexDataList = xmlDoc.Descendants("row").
+                                    Where(x => x.Element("swift_code").Value == "EUR" || x.Element("swift_code").Value == "USD")
+                                   .Select(x => new ForexData
+                                   {
+                                       LanguageCode = x.Element("swift_code").Value,
+                                       Currency = x.Element("CurrencyGuide").Value
+                                   })
+                                   .ToList();
+                }
+                catch (Exception ex)
+                {
+
+                }                
+            });
+            return forexDataList;
+        }     
+        public async Task<JsonResult> GetCurrencyDataAsync()
+        {
+            return Json(await CurrencyData(), JsonRequestBehavior.AllowGet);
+        }
+        public class ForexData
+        {
+            public string LanguageCode { get; set; }
+            public string Currency { get; set; }
+        }
+
+    }
+}
---
+++ ezytask8/Scripts/currency.js
@@ -0,0 +1,59 @@
+﻿var currency = (function () {
+    var currencies = [];
+    var init = function () {
+        setCurrencyButton();
+    }
+    var getCurrencyData = function () {
+        loadCurrencyData(updateCurrencyData);
+    }
+    var updateCurrencyData = function () {
+        var currencyData = document.getElementById("currencyDataBody");
+        currencyData.innerHTML = "";
+        currencies.forEach(function(i, v){
+            var newLiElement = createNewLiElement(i.LanguageCode, i.Currency);
+            currencyData.appendChild(newLiElement);
+        });
+        document.getElementById("loadingText").style.display = "none";
+    }
+    var createNewLiElement = function(languageCode, currency) {
+        var el = document.createElement("tr");
+        var languageSpan = document.createElement("span");
+        var currencySpan = document.createElement("span");
+        languageSpan.innerHTML = languageCode;
+        currencySpan.innerHTML = currency;
+        el.appendChild(languageSpan);
+        el.appendChild(currencySpan);
+        return el;
+    }
+    var loadCurrencyData = function (updateCurrencyData) {
+        document.getElementById("loadingText").style.display = "block";
+        document.getElementById("currencyDataBody").innerHTML = "";
+        var xhr = new XMLHttpRequest();
+        var url = "/Home/GetCurrencyDataAsync"
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function (data) {             
+            var status = xhr.status;
+            if (xhr.readyState == 4 && xhr.status == 200){ 
+                currencies = [];
+                currencies = data.target.response;  
+                updateCurrencyData();      
+            } else {
+                alert("error!");
+            }
+        };
+        xhr.send();
+    };
+
+    var setCurrencyButton = function () {
+        var getCurrencyButton = document.getElementById("getCurrencyData");
+        getCurrencyButton.addEventListener("click", function () {
+            currency.getCurrencyData();
+        });
+    }
+    return {
+        init: init,
+        getCurrencyData: getCurrencyData
+    }
+})();
+currency.init();
